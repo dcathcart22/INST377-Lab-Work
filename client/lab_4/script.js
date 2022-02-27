2cc47e27e7864eca888312e6464756d50bdadbfa
dcathcart22 <dylancathcart@gmail.com>
2022-02-27 16:41:14 -0500

    js

---
+++ client/lab_4/script.js
@@ -0,0 +1,38 @@
+const slideposition = 0;
+const slides = document.getElementsByClassName('carousel_item');
+const totalSlides = slides.length;
+
+const next = document.getElementById('next').addEventListener('click', () => {
+  moveToNextSlide();
+});
+
+const prev = document.getElementById('prev').addEventListener('click', () => {
+  moveToPrevSlide();
+});
+
+function updateSlidePosition() {
+  for (const slide of slides) {
+    slide.classList.remove('carousel_item-visible');
+    slide.classList.add('carousel_item-hidden');
+  }
+
+  slides[slidePosition].classList.add('carousel_item-visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+  updateSlidePosition(slides);
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+  updateSlidePosition(slides);
+}
