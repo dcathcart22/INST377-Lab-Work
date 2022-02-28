6fb981ad002eed246adccdb8416f46ae16d7fd19
dcathcart22 <dylancathcart@gmail.com>
2022-02-27 21:14:58 -0500

    bugs

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,47 +1,61 @@
-/* eslint-disable no-const-assign */
-const slideposition = 0;
+/* eslint-disable func-names */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-plusplus */
+let slidePosition = 0;
 const slides = document.getElementsByClassName('carousel_item');
 const totalSlides = slides.length;
 
-// eslint-disable-next-line no-unused-vars
-const next = document.getElementById('next').addEventListener('click', () => {
-  // eslint-disable-next-line no-use-before-define
-  moveToNextSlide();
-});
+console.log(totalSlides);
 
-const prev = document.getElementById('prev').addEventListener('click', () => {
-  // eslint-disable-next-line no-use-before-define
-  moveToPrevSlide();
-});
+document
+  .getElementById('button--next')
+  // eslint-disable-next-line func-names
+  // eslint-disable-next-line prefer-arrow-callback
+  .addEventListener('click', function() {
+    console.log('next');
+    moveToNextSlide();
+    document.getElementById('button--previous').style.background = 'lightgrey';
+    document.getElementById('button--next').style.background = 'orange';
+    document.getElementById('button--next').style.borderColor = 'green';
+    document.getElementById('button--previous').style.borderColor = 'white';
+  });
+
+document
+  .getElementById('button--previous')
+  .addEventListener('click', () => {
+    console.log('previous');
+    moveToPrevSlide();
+    document.getElementById('button--next').style.background = 'lightgrey';
+    document.getElementById('button--previous').style.background = 'orange';
+    document.getElementById('button--previous').style.borderColor = 'green';
+    document.getElementById('button--next').style.borderColor = 'white';
+  });
 
 function updateSlidePosition() {
-  // eslint-disable-next-line no-restricted-syntax
   for (const slide of slides) {
-    slide.classList.remove('carousel_item-visible');
-    slide.classList.add('carousel_item-hidden');
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
   }
 
-  slides[slideposition].classList.add('carousel_item-visible');
+  slides[slidePosition].classList.add('carousel_item--visible');
 }
 
 function moveToNextSlide() {
-  if (slideposition === totalSlides - 1) {
-    // eslint-disable-next-line no-const-assign
-    slideposition = 0;
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
   } else {
-    // eslint-disable-next-line no-const-assign
-    // eslint-disable-next-line no-plusplus
-    slideposition++;
+    slidePosition++;
   }
-  updateSlidePosition(slides);
+  updateSlidePosition();
 }
 
 function moveToPrevSlide() {
-  if (slideposition === 0) {
-    slideposition = totalSlides - 1;
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
   } else {
-    // eslint-disable-next-line no-plusplus
-    slideposition--;
+    slidePosition--;
   }
-  updateSlidePosition(slides);
+
+  updateSlidePosition();
 }
